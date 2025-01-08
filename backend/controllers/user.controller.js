9475000020a5e03ff96407a2b75d86fb0c146e27
mukhtar4532 <mukhtarhussain4532@gmail.com>
2025-01-09 02:41:22 +0530

    commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,20 +1,51 @@
+import { json } from "express";
 import userModel from "../models/user.model.js";
-import userService from "../services/user.service.js";
+import * as userService from "../services/user.service.js";
 import { validationResult } from "express-validator";
 
 export const createUserController = async (req, res) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty) {
+  if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
   try {
-    const user = await userModel.createUser(req.body);
+    const user = await userService.createUser(req.body);
 
     const token = await user.generateJWT();
 
     res.status(201).json({ user, token });
   } catch (error) {
     res.status(400).send(error.message);
+  }
+};
+
+export const loginController = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(401).json({ errors: "Invalid credentials" });
+    }
+
+    const isMatch = await user.isValidPassword(password);
+
+    if (!isMatch) {
+      return res.status(401).json({ errors: "Invalid credentials" });
+    }
+
+    const token = await user.generateJWT();
+
+    res.status(200).json({ user, token });
+  } catch (error) {
+    return res.status(401).send(err.message);
   }
 };
--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -1,4 +1,4 @@
-import userModel from "../models/user.model";
+import userModel from "../models/user.model.js";
 
 export const createUser = async ({ email, password }) => {
   if (!email || !password) {
